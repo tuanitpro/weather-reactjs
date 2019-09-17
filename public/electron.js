fd9adaa68907fca87346252438bd9de847391ae6
Kane Le <kane.le@nexcel.vn>
2019-09-17 12:47:33 +0700

    electron

---
+++ public/electron.js
@@ -0,0 +1,47 @@
+const {app, BrowserWindow, dialog, shell} = require('electron')
+const path = require("path");
+
+app.showExitPrompt = true
+
+let win;
+
+function createWindow () {  
+win = new BrowserWindow({width: 800, height: 600})
+win.setMenu(null)
+//win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
+win.loadURL('http://localhost:3000/')
+
+win.on('close', (e) => {
+        if (app.showExitPrompt) {
+            e.preventDefault()
+            dialog.showMessageBox({
+                type: 'question',
+                buttons: ['Yes', 'No'],
+                title: 'Confirm',
+                message: 'Are you sure you want to quit?'
+            }, function (response) {
+                if (response === 0) {
+                    app.showExitPrompt = false
+
+                    // shell.openExternal('notepad.exe')
+                    win.close()
+                }
+            })
+        }
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+  });
+
+app.on("activate", () => {
+if (win === null) {    
+    createWindow();
+}
+
+});
